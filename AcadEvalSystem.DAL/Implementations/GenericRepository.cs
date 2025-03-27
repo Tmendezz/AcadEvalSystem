2b00a6a83ea9605192f8c28fbd9c0c09b38a4855
Tiziano Mendez <166566359+Tmendezz@users.noreply.github.com>
2025-03-27 20:20:39 -0300

    Primer Commit

---
+++ AcadEvalSystem.DAL/Implementations/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using AcadEvalSystem.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadEvalSystem.DAL.Implementations
+{
+    internal class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+    }
+}
---
+++ AcadEvalSystem.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadEvalSystem.DAL.Interfaces
+{
+    internal interface IGenericRepository<T> where T : class
+    {
+    }
+}
